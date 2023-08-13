9a0b358f30fda48c8b0fd40fae0a27883ff63dde
Balázs Klement <6020706+bklement@users.noreply.github.com>
2023-08-13 01:10:28 +0000

    Add line ending

--- install.py
+++ install.py
@@ -6,7 +6,7 @@ config = Config(os.path.expanduser('~') + "/.gitargus/config.yml")
 for repository in config.repositories():
     filename = config.root() + "/" + repository + "/.git/hooks/pre-push"
     f = open(filename, "w")
-    f.write("#!" + config.getProperty("python.bin"))
+    f.write("#!" + config.getProperty("python.bin") + "\n")
     f.write("from gitargus import hooks\n\n")
     f.write("hooks.pre_push()\n")
     f.close()
