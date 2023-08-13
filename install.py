3f8f4a527d8f59247fc56cd3d15eea622e6b98a6
Balázs Klement <6020706+bklement@users.noreply.github.com>
2023-08-13 01:09:11 +0000

    Extract python bin location to config

--- install.py
+++ install.py
@@ -3,13 +3,10 @@ from gitargus.core import Config, CLI
 
 config = Config(os.path.expanduser('~') + "/.gitargus/config.yml")
 
-pythonPath = CLI("/").run(["which", "python3"])
-print(pythonPath)
-
 for repository in config.repositories():
     filename = config.root() + "/" + repository + "/.git/hooks/pre-push"
     f = open(filename, "w")
-    f.write("#!" + pythonPath)
+    f.write("#!" + config.getProperty("python.bin"))
     f.write("from gitargus import hooks\n\n")
     f.write("hooks.pre_push()\n")
     f.close()
