a68168cf811f6aa7507cd91d154cf2074e39b8c8
Balázs Klement <6020706+bklement@users.noreply.github.com>
2023-12-07 20:00:33 +0100

    Creating venv, adding install functionality

--- src/gitargus/__main__.py
+++ src/gitargus/__main__.py
@@ -1,12 +1,29 @@
-from .core import Workspace, Config, Dynamodb
+from core import Workspace, Config, Dynamodb
+import sys
 
 
-def run():
-    config = Config()
-    dynamodb = Dynamodb(config.hostname(), config.table())
-    workspace = Workspace(config.root(), config.repositories(), config.timezone())
-    dynamodb.save(workspace.readRepositoryStatuses())
+config = Config()
+workspace = Workspace(config.root(), config.repositories(), config.timezone())
+
+
+def install():
+    workspace.install()
+
+
+def update(fetch: bool):
+    print("update, fetch: ", fetch)
+    Dynamodb(config.hostname(), config.table()).save(workspace.readRepositoryStatuses(fetch))
 
 
 if __name__ == '__main__':
-    run()
+    if (sys.argv[1] == "install"):
+        install()
+    elif (sys.argv[1] == "update"):
+        if (len(sys.argv) == 2):
+            update(False)
+        elif (sys.argv[2] == "--fetch"):
+            update(True)
+        else:
+            print("Unrecognized option " + sys.argv[2])
+    else:
+        print("Unrecognized command " + sys.argv[1])
--- src/gitargus/core.py
+++ src/gitargus/core.py
@@ -20,6 +20,19 @@ def log(s: str):
     logging.info(s)
 
 
+def parse(header: str):
+    if header.endswith("]"):
+        if ("ahead" in header):
+            if ("behind" in header):
+                return "DIVERGED"
+            else:
+                return "AHEAD"
+        else:
+            return "BEHIND"
+    else:
+        return "UP_TO_DATE"
+
+
 class Config():
 
     def __init__(self):
@@ -54,19 +67,29 @@ class CLI():
     def __init__(self, folder: str):
         self.__folder = folder
 
-    def run(self, params):
+    def createFolder(self):
+        try:
+            p = run(["mkdir", "-p", self.__folder], check=True, capture_output=True, text=True)
+            return p.stdout
+        except CalledProcessError as e:
+            log("Error creating folder {}:\n{}".format(self.__folder, e))
+        except FileNotFoundError:
+            log("Tried to run command '{}', but it does not exist.".format("mkdir"))
+
+    def run(self, params: list, supressErrors: bool = False):
         try:
             os.chdir(self.__folder)
         except FileNotFoundError:
             log("Tried to change to folder '{}' but it does not exist.".format(self.__folder))
-            return None
         try:
             p = run(params, check=True, capture_output=True, text=True)
             return p.stdout
         except CalledProcessError as e:
-            log("Error running subprocess '{}' in '{}':\n{}".format(" ".join(params), os.getcwd(), e))
+            if not supressErrors:
+                log("Error running subprocess '{}' in '{}':\n{}".format(" ".join(params), os.getcwd(), e))
         except FileNotFoundError:
-            log("Tried to run command '{}', but it does not exist.".format(params[0]))
+            if not supressErrors:
+                log("Tried to run command '{}', but it does not exist.".format(params[0]))
 
 
 class Dynamodb():
@@ -78,7 +101,6 @@ class Dynamodb():
 
     def save(self, results: dict):
         log("Saving to dynamodb.")
-        # log("Payload to dynamodb: {}".format(dict))
         item = {"hostname": self.__hostname}
         item.update(results)
         boto3.resource("dynamodb").Table(self.__table).put_item(TableName=self.__table, Item=item)
@@ -86,18 +108,34 @@ class Dynamodb():
 
 class Repository():
 
-    def __init__(self, root: str, name: str, timezone: str):
-        self.__folder = root + "/" + name
-        self.__cli = CLI(self.__folder)
+    def __init__(self, parent: str, name: str, origin: str):
+        self.__parentFolder = parent
         self.__name = name
-        self.__timezone = timezone
+        self.__folder = parent + "/" + name
+        self.__origin = origin
 
-    def __timestamp(self):
-        return datetime.now(pytz.timezone(self.__timezone)).strftime("%Y-%m-%d %H:%M:%S")
+    def __run(self, params: list, folder: str = None, supressErrors: bool = False):
+        try:
+            if folder is None:
+                os.chdir(self.__folder)
+            else:
+                os.chdir(folder)
+        except FileNotFoundError:
+            log("Tried to change to folder '{}' but it does not exist.".format(self.__folder))
+            return None
+        try:
+            p = run(params, check=True, capture_output=True, text=True)
+            return p.stdout
+        except CalledProcessError as e:
+            if not supressErrors:
+                log("Error running subprocess '{}' in '{}':\n{}".format(" ".join(params), os.getcwd(), e))
+        except FileNotFoundError:
+            if not supressErrors:
+                log("Tried to run command '{}', but it does not exist.".format(params[0]))
 
-    def __pull(self):
+    def pull(self):
         log("Pulling repository {} if fast-forwarding is possible.".format(self.__name))
-        outcome = self.__cli.run(["git", "pull", "--ff-only"])
+        outcome = self.__run(["git", "pull", "--ff-only"])
         if outcome == "fatal: Not possible to fast-forward, aborting.":
             log("Fast-forward failed.")
             return False
@@ -107,26 +145,14 @@ class Repository():
             log("Successfully fast-forwarded.")
             return True
 
-    def __getState(self, header):
-        if header.endswith("]"):
-            if ("ahead" in header):
-                if ("behind" in header):
-                    return "DIVERGED"
-                else:
-                    return "AHEAD"
-            else:
-                return "BEHIND"
-        else:
-            return "UP_TO_DATE"
-
-    def getStatus(self):
-        log("Fetching repository {}".format(self.__name))
-        self.__cli.run(["git", "fetch", "--all"])
+    def getStatus(self, fetch: bool = True):
+        if fetch:
+            log("Fetching repository {}".format(self.__name))
+            self.__run(["git", "fetch", "--all"])
         log("Reading repository status for {}".format(self.__name))
-        stdout = self.__cli.run(["git", "status", "-sb"])
+        stdout = self.__run(["git", "status", "-sb"])
         if stdout is None:
             return {self.__name: {
-                "timestamp": self.__timestamp(),
                 "state": "FAILED_UPDATE"
             }}
         else:
@@ -134,18 +160,21 @@ class Repository():
             result.remove("")
             header = result[0].replace("## ", "").replace("\n", "").split("...")
             local = header[0]
-            remote = header[1].split(" ")[0]
-            state = self.__getState(header[1])
-            if (state == "BEHIND" or state == "DIVERGED"):
-                if (self.__pull()):
-                    return self.getStatus()
+            if len(header) == 1:
+                remote = None
+                state = "LOCAL_ONLY"
+            else:
+                remote = header[1].split(" ")[0]
+                state = parse(header[1])
+                if (state == "BEHIND" or state == "DIVERGED"):
+                    if (self.pull()):
+                        return self.getStatus()
             changes = result[1:]
             if changes:
                 clean = False
             else:
                 clean = True
             return {self.__name: {
-                "timestamp": self.__timestamp(),
                 "local": local,
                 "remote": remote,
                 "state": state,
@@ -153,21 +182,53 @@ class Repository():
                 "changes": changes
             }}
 
+    def install(self):
+        if os.path.exists(self.__parentFolder):
+            log("Parent folder already exists: {}, skipping.".format(self.__parentFolder))
+        else:
+            try:
+                p = run(["mkdir", "-p", self.__folder], check=True, capture_output=True, text=True)
+                return p.stdout
+            except CalledProcessError as e:
+                log("Error creating folder {}:\n{}".format(self.__folder, e))
+            except FileNotFoundError:
+                log("Tried to run command '{}', but it does not exist.".format("mkdir"))
+        if not os.path.exists(self.__folder) or self.__run(["git", "status"], self.__folder, True) is None:
+            log("Pulling repository {}...".format(self.__name))
+            self.__run(["git", "clone", self.__origin], self.__parentFolder)
+            log("Success.")
+        else:
+            log("Tried to pull already present repository: {}, skipping.".format(self.__folder))
+
 
 class Workspace():
 
-    def __init__(self, root: str, repositoryNames: list, timezone: str):
-        self.__repositories = {}
-        for name in repositoryNames:
-            if os.path.exists(root + "/" + name):
-                self.__repositories.update({
-                    name: Repository(root, name, timezone)
-                })
-            else:
-                log("Repository {} does not exists on the filesystem in workspace {}.".format(name, root))
+    def __init__(self, root: str, repositories: dict, timezone: str):
+        if os.path.exists(root):
+            log("Found root directory {}".format(root))
+        else:
+            log("Root does not exists on the filesystem, exiting.")
+            exit(-1)
+        self.__timezone = pytz.timezone(timezone)
+        self.__repositories = []
+        self.__dict = {}
+        self.__extractRepositories(root, repositories)
 
-    def readRepositoryStatuses(self):
-        results = {}
-        for _, repository in self.__repositories.items():
-            results.update(repository.getStatus())
-        return results
+    def __extractRepositories(self, parent: str, dictionary: dict):
+        for key in dictionary.keys():
+            if isinstance(dictionary[key], dict):
+                self.__extractRepositories(parent + "/" + key, dictionary[key])
+            else:
+                self.__repositories.append(Repository(parent, key, dictionary[key]))
+
+    def readRepositoryStatuses(self, fetch: bool = True):
+        statuses = {}
+        for repository in self.__repositories:
+            status = repository.getStatus(fetch)
+            status[list(status.keys())[0]].update({"timestamp": datetime.now(self.__timezone).strftime("%Y-%m-%d %H:%M:%S")})
+            statuses.update(status)
+        return statuses
+
+    def install(self):
+        for repository in self.__repositories:
+            repository.install()
